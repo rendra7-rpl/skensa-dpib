e6a16a6fb2157199a6e2a60041ca8edf14c09db4
rendra7-rpl <pratamarendra904@gmail.com>
2024-11-15 07:02:16 +0700

    informasi-dpib-skensa

--- sign in up.js
+++ sign in up.js
@@ -1,6 +1,8 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
+const emailInput = document.querySelector('.sign-in input[type="email"]');
+const signInButton = document.querySelector('.btn-signin');
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
@@ -9,9 +11,18 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
 document.querySelector('.btn-signup').addEventListener('click', () => {
     window.location.href = 'sign in up.html'; // Ganti dengan URL beranda Anda
 });
-document.querySelector('.btn-signin').addEventListener('click', () => {
-    window.location.href = 'homepage.html'; // Ganti dengan URL beranda Anda
+
+signInButton.addEventListener('click', (e) => {
+    e.preventDefault(); // Mencegah form submit otomatis
+
+    const email = emailInput.value;
+    if (!email.endsWith('@gmail.com')) {
+        alert("Hanya bisa login dengan akun Gmail.");
+    } else {
+        window.location.href = 'homepage.html'; // Ganti dengan URL beranda Anda
+    }
 });
